622e68cae9f59b26dda021bde4efe6951b564390
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-07 11:44:42 +0200

    class hovercraft extends IsLandVehicleClass + implements + Ajout des methodes

--- ExosInterfaceVehicule070818/src/package1/Hovercraft.java
+++ ExosInterfaceVehicule070818/src/package1/Hovercraft.java
@@ -1,5 +1,30 @@
 package package1;
 
-public class Hovercraft {
+public class Hovercraft extends IsLandVehicleClass implements IsLandVehicle, IsSeaVessel {
+	// Une classe peut implémenter deux interfaces mais elle ne peut pas hériter de deux classes
+	// On rajoute les méthodes "à la main" de la classe IsSeaVesselClass
+	
+	// Attributs de la classe 
+	
+	int displacement;
 
+	// Méthodes de la classe 
+	
+	public int getDisplacement() {
+		return displacement;
+	}
+
+	public void setDisplacement(int displacement) {
+		this.displacement = displacement;
+	}
+	
+	public void enterLand() {
+		System.out.println("Je glisse sur terre.");
+	}
+	
+	public void enterSea() {
+		System.out.println("Je glisse sur l'eau ! ");
+	}
+	
+	
 }
