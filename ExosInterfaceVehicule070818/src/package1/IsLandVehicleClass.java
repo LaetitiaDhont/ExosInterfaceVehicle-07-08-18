f4b19eb38c428b5040a7764c26899859874db9bf
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-07 11:11:58 +0200

    extends class implements interfaces

---
+++ ExosInterfaceVehicule070818/src/package1/IsLandVehicleClass.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class IsLandVehicleClass extends Vehicle implements IsLandVehicle {
+// Extends pour l'héritage entre deux classes et implements pour une classe avec une interface
+// La classe IsLandVesselClass hérite de la classe Vehicle 
+// La classe IsLandVesselClass implémente l'interface IsLandVessel
+	
+	//Attribut de la classe
+	int numWheels;
+
+	// Méthodes de la classe 
+
+	public int getNumWheels() {
+		return numWheels;
+	}
+
+	public void setNumWheels(int numWheels) {
+		this.numWheels = numWheels;
+	}
+	
+	
+	
+}
---
+++ ExosInterfaceVehicule070818/src/package1/IsSeaVesselClass.java
@@ -0,0 +1,17 @@
+package package1;
+
+// La classe IsSeaVesselClass hérite dee classe Vehicle et elle implémente l'interface IsSeaVessel
+public class IsSeaVesselClass extends Vehicle implements IsSeaVessel {
+	
+	//Attributs de la classe IsSeaVesselClass
+	int displacement;
+
+	// Méthodes de la classe 
+	public int getDisplacement() {
+		return displacement;
+	}
+
+	public void setDisplacement(int displacement) {
+		this.displacement = displacement;
+	}
+}
